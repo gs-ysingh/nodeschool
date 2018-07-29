d22d2f30f1763e7bdb7b0a01a1d51811b8ff0a92
Yogesh Singh <ysingh@nerdwallet.com>
2018-07-30 00:25:16 +0530

    refactor

--- learnyounode.js
+++ learnyounode.js
@@ -2,19 +2,12 @@
 
 console.log("HELLO WORLD");
 
-//2 - Write a program that accepts one or more numbers as command-line arguments and prints the sum of 
-//those numbers to the console (stdout).
-
-var sum = 0;
-for(var i = 2; i < process.argv.length; i++) {
-	sum += Number(process.argv[i]);
-}
-console.log(sum);
+//2 - sum.js
 
 //3 - Write a program that uses a single synchronous filesystem operation to read a file and print 
 //the number of newlines it contains to the console (stdout), similar to running cat file | wc -l.
 
-The full path to the file to read will be provided as the first command-line argument.
+// The full path to the file to read will be provided as the first command-line argument.
 var fs = require('fs');
 var buf = fs.readFileSync(process.argv[2]);
 console.log(buf.toString().split('\n').length - 1);
---
+++ sum.js
@@ -0,0 +1,8 @@
+//2 - Write a program that accepts one or more numbers as command-line arguments and prints the sum of 
+//those numbers to the console (stdout).
+
+var sum = 0;
+for(var i = 2; i < process.argv.length; i++) {
+	sum += Number(process.argv[i]);
+}
+console.log(sum);
